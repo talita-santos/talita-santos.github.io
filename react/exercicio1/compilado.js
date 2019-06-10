91b5411d183863edf59f18e725074feb3f4da65a
talita-santos <talita.os@hotmail.com>
2019-06-10 17:39:48 -0300

    react

---
+++ react/exercicio1/compilado.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var app = document.getElementById('app');
+var template = React.createElement("h1", null, " deu certo");
+ReactDOM.render(template, app);
---
+++ react/exercicio1/script.js
@@ -0,0 +1,5 @@
+
+const app = document.getElementById('app')
+const template = <h1> deu certo</h1>
+ReactDOM.render(template,app)
+
